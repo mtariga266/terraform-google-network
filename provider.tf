c6ca156c1f33184b8233a53462dc9ef33cc7a2e9
mtariga266 <126924464+mtariga266@users.noreply.github.com>
2023-03-31 00:45:44 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.57.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "my-second-project-381922"
+  region      = "northamerica-northeast1"
+}
+
+
+terraform {
+  cloud {
+    organization = "mhay0805yy"
+
+    workspaces {
+      name = "terraform-google-network"
+    }
+  }
+}
