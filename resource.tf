2c8ee5d92f51216f54c7bb47ba47cfe3ee87d292
mtariga266 <126924464+mtariga266@users.noreply.github.com>
2023-03-31 00:20:27 -0400

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,11 @@
+Resource:
+resource "google_compute_network" "vpc_network" {
+  name = "vpc-network"
+}
+
+resource "google_compute_network" "vpc_network" {
+  project                 = "my-second-project-381922"
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
